82c1ca03c335c71200a781d1a9522e6da312e426
s0chu <didididi328@gmail.com>
2025-08-16 21:12:01 +0300

    done CRC

--- chunks.py
+++ chunks.py
@@ -1,30 +1,38 @@
+from collections import deque
+
 class Chunk:
     def CRC_validator(this):
         bin = ''.join(''.join(reversed(format(ord(j) , "08b"))) for j in this.name)
-        bin = bin + ''.join(  ''.join(reversed(format(j , "08b"))) for j in this.data)
+        bin = bin + ''.join(''.join(reversed(format(j , "08b"))) for j in this.data)
         bin = [(ord(j) - ord('0')) for j in bin]
-
-
-        divisor = [0 for j in range(0 , 33)]
         
-        for j in [32 , 26 , 23 , 22 , 16 , 12 , 11 , 10 , 8 , 7 , 5 , 4 , 2 , 1 , 0]:
+  
+        divisor = [0 for j in range(0 , 32)]
+
+        for j in [26 , 23 , 22 , 16 , 12 , 11 , 10 , 8 , 7 , 5 , 4 , 2 , 1 , 0]:
             divisor[j] = 1
         
-        for i in range(0 , len(bin)):
-            if(bin[i] == 1 and i + len(divisor) - 1 < len(bin)):
-                for j in range(i , i + len(divisor)):
-                    bin[j] = bin[j] ^ divisor[j - i]    
+        divisor = [i for i in reversed(divisor)]
+  
+  
+        rem = [1 for i in range(0 , 32)]
 
-        rem = [bin[i] for i in range((0 if 32 > len(bin) else len(bin) - 32) , len(bin))]
-        print(f"dimension is {len(rem)}")
+        for i in bin:
+            rem[0] ^= i
+            msb = rem.pop(0)
+            rem.append(0)
+
+            if(msb == 1):
+                for i in range(0 , 32):
+                    rem[i] = rem[i] ^ divisor[i]
+
+        rem = [i for i in reversed(rem)]
 
         for i in range(0 , 32):
             rem[i] = 1 - rem[i]
 
-        crc = ''.join(reversed(''.join(chr(i + ord('0')) for i in rem)))
-        print(int(crc , 2))
-        print(this.CRC)
-        return 1
+        crc = ''.join(''.join(chr(i + ord('0')) for i in rem))
+        return int(crc , 2) == this.CRC
 
     def __init__(this , file):
         this.len = int.from_bytes(file.read(4) , 'big')
@@ -36,4 +44,3 @@ class Chunk:
             raise Exception("CRC invalid, send was compromised")
     
 
-    
