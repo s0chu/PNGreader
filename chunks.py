db6791e48bad3bf7e5593948c5dc1f12e59fc1bc
s0chu <didididi328@gmail.com>
2025-08-17 09:02:36 +0300

    read zlib format

--- chunks.py
+++ chunks.py
@@ -43,4 +43,62 @@ class Chunk:
         if(this.CRC_validator() == 0):
             raise Exception("CRC invalid, send was compromised")
     
+    def init(this , chunk):
+        this.len = chunk.len
+        this.name = chunk.name
+        this.data = chunk.data
+        this.CRC = chunk.CRC
 
+
+
+class IDAT(Chunk):
+    def __init__(this , chunk):
+        super().init(chunk)
+
+class Decoder:
+    def __init__(this):
+        this.idat = []
+        this.chunk_type = { "IDAT" : this.idat }
+        this.chunk_init = { "IDAT" : IDAT }
+
+    def process(this , chunk):
+
+        if chunk.name != "IDAT" : 
+            return 0
+        
+        if chunk.name not in this.chunk_type:
+            raise Exception("Chunk name unknwown")
+        
+        this.chunk_type[chunk.name].append(this.chunk_init[chunk.name](chunk))
+
+    def solve_idat(this):
+        data = b''
+
+        for c in this.idat:
+            data += c.data
+        
+        CMF = data[0]
+        CM = (CMF & 0xF)
+        CINFO = (CMF >> 4)
+
+        if CM != 8:
+            raise Exception("Unknown compression method")
+        
+        if CINFO != 7: 
+            raise Exception("Unkonwn base")
+        
+        FLG = data[1]
+        FCHECK = (FLG & 0x1F)
+        FDICT = (FLG >> 5 & 1)
+        FLEVEL = (FLG >> 6)
+
+        if (CMF * 256 + FLG) % 31 != 0:
+            raise Exception("FCHECK doesn't match")
+
+        if FDICT == 1:
+            raise Exception("Dict used")
+        
+
+        ADLER32 = int.from_bytes(data[len(data) - 4 : ] , 'big')
+        data = data[2 : data[len(data) - 4]]
+        
--- main.py
+++ main.py
@@ -5,10 +5,14 @@ file = open(sys.argv[1] , "rb")
 
 signature = file.read(8)
 
+decoder = chunks.Decoder()
+
 while(1):
     chunk = chunks.Chunk(file)
-
+    decoder.process(chunk)
     print(chunk.name)
 
     if(chunk.name == "IEND"):
         break
+
+decoder.solve_idat()
